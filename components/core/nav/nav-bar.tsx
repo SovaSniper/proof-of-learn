5fa98672b98e4a7c2b83b18088ad35951cf20caf
5208980 <z5208980@zmail.unsw.edu.au>
2024-08-27 20:09:27 +1000

    update

--- components/core/nav/nav-bar.tsx
+++ components/core/nav/nav-bar.tsx
@@ -1,23 +1,27 @@
 "use client";
 
 import { ThemeToggle } from '@/components/theme/theme-toggle';
-import { ConnectWallet } from './connect-wallet';
 import { useRouter } from 'next/navigation';
-import { Ribbon } from 'lucide-react';
+import { OCIDConnect } from './ocid-connect';
+import Image from 'next/image';
 
 interface NavBarProps extends React.HTMLAttributes<HTMLDivElement> {
 }
 
 export function NavBar({ children }: NavBarProps) {
     const router = useRouter();
+
     return <div className="flex items-center justify-between sticky top-2 mx-4 px-8 h-14 z-50 rounded-lg bg-grayscale-025">
-        <div className="flex items-center gap-1 hover:cursor-pointer hover:text-primary" onClick={() => router.push("/")}>
-            <Ribbon size={26} className="text-primary" />
-            <div className="text-xl font-heading hidden sm:block">Proof of Learn</div>
+        <div className="flex items-center gap-2 hover:cursor-pointer hover:text-primary" onClick={() => router.push("/")}>
+            <div>
+                <Image src="/badge.svg" height={24} width={24} alt="logo" />
+            </div>
+            <div className="text-xl font-semibold hidden sm:block">Proof of Learn</div>
         </div>
         <div className="flex items-center gap-2">
             <ThemeToggle />
-            <ConnectWallet />
+            {/* <ConnectWallet /> */}
+            <OCIDConnect />
         </div>
     </div>
 };
---
+++ components/core/nav/ocid-connect.tsx
@@ -0,0 +1,40 @@
+'use client';
+
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import { useOCAuth } from '@opencampus/ocid-connect-js';
+import { Button } from '../../ui/button';
+import { ProfileMenu } from './profile-menu';
+import Image from 'next/image';
+
+interface OCIDConnectProps extends React.HTMLAttributes<HTMLDivElement> {
+}
+
+export function OCIDConnect({ children }: OCIDConnectProps) {
+    const { ocAuth, authState } = useOCAuth();
+
+    useEffect(() => {
+        if (authState.isAuthenticated) {
+            // console.log(ocAuth)
+            // console.log(authState)
+        }
+    }, [authState.isAuthenticated]);
+
+    const handleLogin = async () => {
+        try {
+            await ocAuth.signInWithRedirect({ state: 'opencampus' });
+        } catch (error) {
+            console.error('Login error:', error);
+        }
+    };
+
+    return <>
+        {authState.isAuthenticated
+            ? <ProfileMenu address="0x" />
+            : <Button onClick={handleLogin}
+                className="flex items-center gay-2">
+                <Image src="icons/open-campus.svg" alt="logo" height={32} width={32} />
+                Connect OCID
+            </Button>}
+    </>
+}
